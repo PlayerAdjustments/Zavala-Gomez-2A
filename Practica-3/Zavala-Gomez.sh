d24ecda4d803ed47be0bae8f4cef9085430a04ff
Fernando Zavala <94272096+PlayerAdjustments@users.noreply.github.com>
2023-05-13 22:48:01 -0600

    Practica-3 13-Mayo-2023

--- Practica-3/Zavala-Gomez.sh
+++ Practica-3/Zavala-Gomez.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 Choice=-1
 url="Default"
@@ -11,58 +11,58 @@ body="Default"
 
 until [ "$Choice" = "1" ] || [ "$Choice" = "2" ] || [ "$Choice" = "3" ]
 do
-	printf "Seleccione entre las siguientes opciones \n 1. Guardar HTML como palabra \n 2. Consultar informacion de una API \n 3. Postear informaciòn a una API \n Eleccion: "
-	read Choice
+        printf "Seleccione entre las siguientes opciones \n 1. Guardar HTML como palabra \n 2. Consultar informacion de una API \n 3. Postear>
+        read Choice
 done
-
-echo "Seleccionaste: $Choice"
-
 
 if [ "$Choice" = "1" ]
 then
-	until [ "$url" = "NO" ] || [ "$word" = "NO" ]
-	do
-		url="Default"
-		word="Default"
-		while [ "$url" =  "Default" ] || [ -z "$url" ]
-		do
-			printf "Introduzca una URL: "
-			read url
-		done
 
-		while [ "$word" = "Default" ] || [ -z "$word" ]
-		do
-			printf "Introduzca el nombre bajo el que se guardara el archivo: "
-			read word
-		done
-		
-		if [ "$url" = "NO" ] || [ "$word" = "NO" ]
-		then
-			break
-		fi
-		
-		curl $url > $word.html
-		printf "Archivo $word.tar.gz guardado con exito"
-	done
+        until [ "$url" = "NO" ] || [ "$word" = "NO" ]
+        do
+                url="Default"
+                word="Default"
+
+                while [ "$url" =  "Default" ] || [ -z "$url" ]
+                do
+                        printf "Introduzca una URL: "
+                        read url
+                done
+
+                while [ "$word" = "Default" ] || [ -z "$word" ]
+                do
+                        printf "Introduzca el nombre bajo el que se guardara el archivo: "
+                        read word
+                done
+
+                if [ "$url" = "NO" ] || [ "$word" = "NO" ]
+                then
+                        break
+                fi
+
+                curl -L $url > $word.html
+                printf "Archivo $word.tar.gz guardado con exito"
+
+        done
 fi
 
 if [ "$Choice" = "2" ]
 then
-	curl  https://jsonplaceholder.typicode.com/posts -H "Accept: application/json" > posts.json
-	printf "POST Guardado en post.json correctamente"
+        curl  https://jsonplaceholder.typicode.com/posts -H "Accept: application/json" > posts.json
+        printf "POST Guardado en post.json correctamente"
 fi
 
 if [ "$Choice" = "3" ]
 then
-	printf "Introduzca lo siguiente:"
-	printf "\n userID: "
-	read userId
-	printf "\n ID: "
-	read id
-	printf "\n Title: "
-	read title
-	printf "\n Body: "
-	read body
+        printf "Introduzca lo siguiente:"
+        printf "\n userID: "
+        read userId
+        printf "\n ID: "
+        read id
+        printf "\n Title: "
+        read title
+        printf "\n Body: "
+        read body
 
-	curl -X POST https://jsonplaceholder.typicode.com/posts -H 'Content-Type: application/json' -d '{ "userId": "'"$userId"'", "id": "'"$id"'","title": "'"$title"'", "body": "'"$body"'" }' > $title.json
+        curl -X POST https://jsonplaceholder.typicode.com/posts -H 'Content-Type: application/json' -d '{ "userId": "'"$userId"'", "id": "'"$>
 fi
